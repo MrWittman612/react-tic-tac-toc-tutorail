593a1708b5d5c6033cbb4eba2f8daf092ebf0867
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-09-13 19:27:54 -0500

    refactor board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,13 +4,13 @@ import Square from "./Square";
 // import { calculateWinner } from "./calculateWinner";
 
 export default class Board extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     squares: Array(9).fill(null),
-  //     xIsNext: true
-  //   };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
   // handleClick(i) {
   //   const squares = this.state.squares.slice();
   //   if (calculateWinner(squares) || squares[i]) {
